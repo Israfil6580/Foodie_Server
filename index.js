b3d66b8f1d89ed3f353ce5c4bbd69d0584f544fd
Israfil H. <israfilhossainmilon@gmail.com>
2024-05-15 20:36:30 +0600

    create dynamic token for every login ,singup and github user

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const { CommandSucceededEvent, ObjectId } = require("mongodb");
 const app = express();
-const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 4000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster3.cbcuonb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3`;
-
+// jwt
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+// jwt end
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,14 +27,13 @@ const corsConfig = {
 app.use(cors(corsConfig));
 app.use(express.json());
 app.use(cookieParser());
-
 async function run() {
   const foodCollection = client.db("foddie").collection("food");
 
-  // create token (JWT)
-
+  // jwt generate
   app.post("/jwt", async (req, res) => {
     const user = req.body;
+    console.log("dynamic user - ", user);
     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
       expiresIn: "365d",
     });
@@ -45,20 +45,6 @@ async function run() {
       })
       .send({ success: true });
   });
-
-  // remove token when logout
-
-  app.get("/logout", (req, res) => {
-    res
-      .clearCookie("token", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        maxAge: 0,
-      })
-      .send({ success: true });
-  });
-
   try {
     // Define routes inside the try block
     app.get("/food", async (req, res) => {
